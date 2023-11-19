c4a66d4c300113b0cc4744e9751818643c501ee2
realib1 <ibeevpersie@gmail.com>
2023-11-18 22:02:36 -0800

    new twerks

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ for (let i = 0; i < btns.length; i++) {
         let current = document.getElementsByClassName("link-active");
         current[0].className = current[0].className.replace("link-active", "");
         this.className += " link-active";
+    });
+}
 
+// const custom = document.getElementById("custom-ul");
+const custom_btns = getElementsByClassName("about-links");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("applications-active");
+        current[0].className = current[0].className.replace("applications-active", "");
+        this.className += "applications-active";
+        console.log("I am cliked")
     });
 }
 
