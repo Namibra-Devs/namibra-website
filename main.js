d8d3ac7aeadc8f62b1c87e16512839a6ccf620e6
realib1 <ibeevpersie@gmail.com>
2023-11-22 21:40:17 -0800

    Workking on Applicaions

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 // const header = document.getElementById("link-div");
 const btns = document.getElementsByClassName("about-link");
 for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
+    btns[i].addEventListener("click", () => {
         let current = document.getElementsByClassName("link-active");
-        current[0].className = current[0].className.replace("link-active", "");
+        current[0].className = current[0].className.replace("link-active", " ");
         this.className += " link-active";
+    });
+}
+
+const app_btns = document.getElementsByClassName("app-link");
+for (let i = 0; i < app_btns.length; i++) {
+    app_btns[i].addEventListener("click", () => {
+        let current = document.getElementsByClassName("app-active");
+        current[0].className = current[0].className.replace("app-active", " ");
+        this.className += "app-active";
     });
 }
 
