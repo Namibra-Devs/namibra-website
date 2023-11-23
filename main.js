01134542da5e612ceb472e28b018b4907b4cd29b
realib1 <ibeevpersie@gmail.com>
2023-11-23 00:06:24 -0800

    Fixes

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-// window.addEventListener('load', () => {
+window.addEventListener('load', () => {
 
-    
-    // const header = document.getElementById("myTab");
-    // const btns = document.getElementsByClassName("about-link");
-    // for (let i = 0; i < btns.length; i++) {
-    //     btns[i].addEventListener("click", () => {
-    //         let current = document.getElementsByClassName("link-active");
-    //         current[0].className = current[0].className.replace(" link-active", "");
-    //         this.className += " link-active";
-    //     });
-    // }
-    
-    const app_btns = document.getElementsByClassName("app-link");
-    for (let i = 0; i < app_btns.length; i++) {
-            app_btns[i].addEventListener("click", () => {
-                    let current = document.getElementsByClassName("apps-active");
-                    // current[0].className = current[0].className.replace(" apps-active", " ");
-                    this.className += " apps-active";
-                });
-            }
-            
-            
-        // });
+
+
+    let navLinks = document.getElementsByClassName("nav-link");
+
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("click", function () {
+            let current = document.getElementsByClassName("nav-link active");
+            current[0].className = current[0].className.replace(" nav-link active", "");
+            this.className += " nav-link active";
+            console.log("clicked")
+        });
+    }
+
+
+    let btns = document.getElementsByClassName("about-link");
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            let current = document.getElementsByClassName("link-active");
+            current[0].className = current[0].className.replace(" link-active", "");
+            this.className += " link-active";
+        });
+    }
+
+
+    let appbtn = document.getElementsByClassName("app-link");
+
+    for (let i = 0; i < appbtn.length; i++) {
+        appbtn[i].addEventListener("click", function () {
+            let current = document.getElementsByClassName("app-active");
+            current[0].className = current[0].className.replace(" app-active", "");
+            this.className += " app-active";
+        });
+    }
+
+});
