c024fd90841b160f02fd58bb673ecda33f5d5a11
realib1 <ibeevpersie@gmail.com>
2023-11-22 22:44:23 -0800

    Fixed and update

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-// const header = document.getElementById("link-div");
-const btns = document.getElementsByClassName("about-link");
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", () => {
-        let current = document.getElementsByClassName("link-active");
-        current[0].className = current[0].className.replace("link-active", " ");
-        this.className += " link-active";
-    });
-}
+// window.addEventListener('load', () => {
 
-const app_btns = document.getElementsByClassName("app-link");
-for (let i = 0; i < app_btns.length; i++) {
-    app_btns[i].addEventListener("click", () => {
-        let current = document.getElementsByClassName("app-active");
-        current[0].className = current[0].className.replace("app-active", " ");
-        this.className += "app-active";
-    });
-}
-
+    
+    // const header = document.getElementById("myTab");
+    // const btns = document.getElementsByClassName("about-link");
+    // for (let i = 0; i < btns.length; i++) {
+    //     btns[i].addEventListener("click", () => {
+    //         let current = document.getElementsByClassName("link-active");
+    //         current[0].className = current[0].className.replace(" link-active", "");
+    //         this.className += " link-active";
+    //     });
+    // }
+    
+    const app_btns = document.getElementsByClassName("app-link");
+    for (let i = 0; i < app_btns.length; i++) {
+            app_btns[i].addEventListener("click", () => {
+                    let current = document.getElementsByClassName("apps-active");
+                    // current[0].className = current[0].className.replace(" apps-active", " ");
+                    this.className += " apps-active";
+                });
+            }
+            
+            
+        // });
